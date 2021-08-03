780e0e58047305303608f5c882ddd33bb4656cbc
Anatolii Pokhylko <pokhylko@gmail.com>
2021-08-03 21:54:52 +0300

    style: fix displaying elements

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -12,7 +12,7 @@ const reverseSteps = [...configuration.steps].reverse();
 export const Steps = ({ currentStep, hideSteps }) => (
   <div className={cn(
     'game-steps',
-    { 'game-steps_mobile': hideSteps },
+    { 'game-steps--mobile': hideSteps },
   )}
   >
     {reverseSteps.map((step) => (
@@ -20,9 +20,9 @@ export const Steps = ({ currentStep, hideSteps }) => (
         className={cn(
           'game-steps__step',
           {
-            'game-steps__step_inactive': step > configuration.steps[currentStep],
-            'game-steps__step_current': step === configuration.steps[currentStep],
-            'game-steps__step_finished': step < configuration.steps[currentStep],
+            'game-steps__step--inactive': step > configuration.steps[currentStep],
+            'game-steps__step--current': step === configuration.steps[currentStep],
+            'game-steps__step--finished': step < configuration.steps[currentStep],
           },
         )}
         key={step}
